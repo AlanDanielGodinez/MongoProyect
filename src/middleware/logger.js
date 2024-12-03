ccaa629015783c3db0a864681ad2a293cde2b73f
AlanDanielGodinez <aldarodriguezgo@ittepic.edu.mx>
2024-12-03 04:20:06 -0700

    Cambio Logger

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,43 +1,55 @@
-// /src/middleware/logger.js
-// Importar el módulo 'redis' para interactuar con una base de datos Redis
 const redis = require('redis');
+
 const client = redis.createClient({
-url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 });
+
 client.on('error', (err) => {
-console.error('Redis error de conexion:', err);
+    console.error('Redis error de conexion: ', err);
 });
+
 client.connect().then(() => {
-console.log('Conectado-->> Redis');
+    console.log('Conectado a Redis');
 }).catch((err) => {
-console.error('Error conexion a Redis:', err);
+    console.error('Error al conectar a Redis: ', err);
 });
+
 module.exports = (req, res, next) => {
-res.on('finish', async () => {
-if (!client.isOpen) {
-console.error('Redis client -->> No conectado.');
-return;
-}
-const key = `${req.method}:${Date.now()}:${req.originalUrl}`;
-const logEntry = JSON.stringify({
-time: new Date(),
-req: {
-method: req.method,
-url: req.originalUrl,
-headers: req.headers,
-body: req.body
-},
-res: {
-statusCode: res.statusCode,
-statusMessage: res.statusMessage,
-res: res.body
-}
-});
-try {
-await client.set(key, logEntry, 'EX', 60 * 60 * 24);
-} catch (err) {
-console.error('Error al salvar:', err);
-}
-});
-next();
+    let originalSend = res.send;
+    let responseBody;
+
+    res.send = function (body) {
+        responseBody = body;
+        return originalSend.apply(this, arguments);
+    }
+
+    res.on('finish', async () => {
+        if (!client.isOpen) {
+            console.error('Redis client --> No se pudo conectar.');
+            return;
+        }
+        const key = `${req.method}:${Date.now()}:${req.originalUrl}`;
+        const logEntry = JSON.stringify({
+            time: new Date(),
+            req: {
+                method: req.method,
+                url: req.originalUrl,
+                headers: req.headers,
+                body: req.body
+            },
+            res: {
+                statusCode: res.statusCode,
+                statusMessage: res.statusMessage,
+                body: responseBody
+            }
+        });
+
+        try {
+            await client.set(key, logEntry, 'EX', 60 * 60 * 24);
+        } catch (err) {
+            console.error('Error al guardar en Redis: ', err);
+        }
+    });
+
+    next();
 };
